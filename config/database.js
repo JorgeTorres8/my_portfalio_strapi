ffa2787c2302a3d608290e08aa1b92ca47e3c402
Gabriel D'Aquino <gabriel.leonardo.d@gmail.com>
2023-08-21 18:56:31 +0000

    fix: creds in code

--- config/database.js
+++ config/database.js
@@ -1,20 +1,22 @@
+require('dotenv')
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: 'mysql',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.c9gy1io.mongodb.net'),
+        host: env('DATABASE_HOST', process.env.DB_HOST),
         srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'myportfolio_strapi'),
-        username: env('DATABASE_USERNAME', 'JTorres_DB'),
-        password: env('DATABASE_PASSWORD', 'laM5rg3FHGePKKAH'),
+        port: env.int('DATABASE_PORT', process.env.DB_PORT),
+        database: env('DATABASE_NAME', process.env.DB_NAME),
+        username: env('DATABASE_USERNAME', process.env.DB_USERNAME),
+        password: env('DATABASE_PASSWORD', process.env.DB_PASSWORD),
       },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
+      //options: {
+        //authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        //ssl: env.bool('DATABASE_SSL', true),
+      //},
     },
   },
 });
